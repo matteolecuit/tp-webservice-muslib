c4634d0728e49874a9d33ac29e82a91709695d2d
Matteo Lecuit <matteo@lecuit.fr>
2020-11-12 10:52:33 +0100

    add global style

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import HomePage from "./components/page/HomePage";
 import reportWebVitals from "./reportWebVitals";
+import GlobalStyle from "./styles/globalStyle";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <HomePage />
   </React.StrictMode>,
   document.getElementById("root")
@@ -13,5 +14,5 @@ ReactDOM.render(
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// or send to an analytics endpoint. Learn ore: https://bit.ly/CRA-vitals
 reportWebVitals();
