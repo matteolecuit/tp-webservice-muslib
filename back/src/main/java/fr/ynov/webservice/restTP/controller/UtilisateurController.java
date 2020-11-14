e4a25ce7dbf6747213b5d60b408fc81b272af6b1
Hugo <hugo.huet.14@gmail.com>
2020-11-14 19:55:30 +0100

    update utilisateur data

--- back/src/main/java/fr/ynov/webservice/restTP/controller/UtilisateurController.java
+++ back/src/main/java/fr/ynov/webservice/restTP/controller/UtilisateurController.java
@@ -1,9 +1,6 @@
 package fr.ynov.webservice.restTP.controller;
 
-import fr.ynov.webservice.restTP.entity.Album;
-import fr.ynov.webservice.restTP.entity.Artiste;
-import fr.ynov.webservice.restTP.entity.Titre;
-import fr.ynov.webservice.restTP.entity.Utilisateur;
+import fr.ynov.webservice.restTP.entity.*;
 import fr.ynov.webservice.restTP.model.Favoris;
 import fr.ynov.webservice.restTP.service.UtilisateurService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,48 +27,73 @@ public class UtilisateurController {
         return userOpt.orElse(null);
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "")
-    public Utilisateur create(@RequestBody Utilisateur utilisateur){
+    @RequestMapping(method = RequestMethod.POST, value = "/register")
+    public Utilisateur createUtilisateur(@RequestBody Utilisateur utilisateur){
         return utilisateurService.save(utilisateur);
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/{id}/favoris/album")
-    public Utilisateur addAlbum(@PathVariable(value = "id") long userId, @RequestBody Album album){
-        return this.utilisateurService.addAlbumToPlaylist(userId, album);
-    }
-
-    @RequestMapping(method = RequestMethod.POST, value = "/{id}/favoris/artiste")
-    public Utilisateur addArtiste(@PathVariable(value = "id") long userId, @RequestBody Artiste artiste){
-        return this.utilisateurService.addArtisteToPlaylist(userId, artiste);
-    }
-
-    @RequestMapping(method = RequestMethod.POST, value = "/{id}/favoris/titre")
-    public Utilisateur addTitre(@PathVariable(value = "id") long userId, @RequestBody Titre titre){
-        return this.utilisateurService.addTitreToPlaylist(userId, titre);
+    public Utilisateur addAlbumToFavorite(@PathVariable(value = "id") long userId, @RequestParam long albumId){
+        return this.utilisateurService.addAlbumToFavorite(userId, albumId);
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "/{id}/favoris/album")
-    public Utilisateur deleteAlbum(@PathVariable(value = "id") long userId, @RequestParam long albumId){
-        return this.utilisateurService.deleteAlbumToPlaylist(userId, albumId);
+    public Utilisateur deleteAlbumFromFavorite(@PathVariable(value = "id") long userId, @RequestParam long albumId){
+        return this.utilisateurService.deleteAlbumFromFavorite(userId, albumId);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/{id}/favoris/artiste")
+    public Utilisateur addArtisteToFavorite(@PathVariable(value = "id") long userId, @RequestParam long artisteId){
+        return this.utilisateurService.addArtisteToFavorite(userId, artisteId);
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "/{id}/favoris/artiste")
-    public Utilisateur deleteArtiste(@PathVariable(value = "id") long userId, @RequestParam long artisteId){
-        return this.utilisateurService.deleteArtisteToPlaylist(userId, artisteId);
+    public Utilisateur deleteArtisteFromFavorite(@PathVariable(value = "id") long userId, @RequestParam long artisteId){
+        return this.utilisateurService.deleteArtisteFromFavorite(userId, artisteId);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/{id}/favoris/titre")
+    public Utilisateur addTitreToFavorite(@PathVariable(value = "id") long userId, @RequestParam long titreId){
+        return this.utilisateurService.addTitreToFavorite(userId, titreId);
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "/{id}/favoris/titre")
-    public Utilisateur deleteTitre(@PathVariable(value = "id") long userId, @RequestParam long titreId){
-        return this.utilisateurService.deleteTitreToPlaylist(userId, titreId);
+    public Utilisateur deleteTitreFromFavorite(@PathVariable(value = "id") long userId, @RequestParam long titreId){
+        return this.utilisateurService.deleteTitreFromFavorite(userId, titreId);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/{id}/favoris")
-    public Favoris addTitre(@PathVariable(value = "id") long userId){
-        Optional<Utilisateur> userOpt = this.utilisateurService.findById(userId);
-        if (userOpt.isPresent()){
-            Utilisateur user = userOpt.get();
-            return new Favoris(user.getTitres(), user.getArtistes(), user.getAlbums());
-        }
-        return null;
+    public Favoris getFavoris(@PathVariable(value = "id") long userId){
+        return this.utilisateurService.getFavoris(userId);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/{userId}/playlist")
+    public Utilisateur createPlaylist(@PathVariable("userId") long userId, @RequestBody Playlist playlist){
+        return this.utilisateurService.createPlaylist(userId, playlist);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/{userId}/playlist/{playId}")
+    public Utilisateur addTitreToPlaylist(@PathVariable(value = "userId") long userId, @PathVariable(value = "playId") long playId, @RequestParam long titreId){
+        return this.utilisateurService.addTitreToPlaylist(userId, playId, titreId);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/{userId}/playlist/{playId}")
+    public Utilisateur deleteTitreFromPlaylist(@PathVariable(value = "userId") long userId, @PathVariable(value = "playId") long playId, @RequestParam long titreId){
+        return this.utilisateurService.deleteTitreFromPlaylist(userId, playId, titreId);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/{userId}/playlist")
+    public Utilisateur deletePlaylist(@PathVariable(value = "userId") long userId, @RequestParam long playId){
+        return this.utilisateurService.deletePlaylist(userId, playId);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{userId}/playlist/{playId}")
+    public Playlist getPlaylist(@PathVariable(value = "userId") long userId, @PathVariable(value = "playId") long playId){
+        return this.utilisateurService.getPlaylist(userId, playId);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{userId}/playlist")
+    public List<Playlist> getAllPlaylist(@PathVariable(value = "userId") long userId){
+        return this.utilisateurService.getAllPlaylist(userId);
     }
 }
