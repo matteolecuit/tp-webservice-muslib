07f4277bba1c04025d45004330cd35fa32e024dc
Hugo <hugo.huet.14@gmail.com>
2020-11-19 11:41:00 +0100

    add login route access

--- back/src/main/java/fr/ynov/webservice/restTP/security/WebSecurity.java
+++ back/src/main/java/fr/ynov/webservice/restTP/security/WebSecurity.java
@@ -27,6 +27,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll()
+                .antMatchers(HttpMethod.POST, SecurityConstants.SIGN_IN_URL).permitAll()
                 .antMatchers(HttpMethod.GET, SecurityConstants.INIT_URL).permitAll()
                 .antMatchers(HttpMethod.GET, SecurityConstants.ALBUM_RANDOM_URL).permitAll()
                 .antMatchers(HttpMethod.GET, SecurityConstants.ARTISTE_RANDOM_URL).permitAll()
