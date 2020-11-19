110ec2fc5d378b5d77553dc3d0cf381a18300f2e
Hugo <hugo.huet.14@gmail.com>
2020-11-19 10:33:00 +0100

    add open init route

--- back/src/main/java/fr/ynov/webservice/restTP/security/WebSecurity.java
+++ back/src/main/java/fr/ynov/webservice/restTP/security/WebSecurity.java
@@ -27,6 +27,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll()
+                .antMatchers(HttpMethod.GET, SecurityConstants.INIT_URL).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
