19083af411bf48a10cb00d7bc42432f9b16f82b1
Hugo <hugo.huet.14@gmail.com>
2020-11-19 10:15:34 +0100

    add import list

--- back/src/main/java/fr/ynov/webservice/restTP/controller/InitController.java
+++ back/src/main/java/fr/ynov/webservice/restTP/controller/InitController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Calendar;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "init")
