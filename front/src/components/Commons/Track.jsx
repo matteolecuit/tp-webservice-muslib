0e1d55f31dd100451e506cdf86baae33398171a2
Matteo Lecuit <matteo@lecuit.fr>
2020-11-20 15:02:39 +0100

    create playlist

--- front/src/components/Commons/Track.jsx
+++ front/src/components/Commons/Track.jsx
@@ -1,60 +1,61 @@
 import React from "react";
 import styled from "styled-components";
-import Icon from 'react-eva-icons';
+import Icon from "react-eva-icons";
 import { Link } from "react-router-dom";
-import AddPlaylistModalForm from "../Modals/AddPlaylistModal";
+import AddToPlaylistModalForm from "../Modals/AddToPlaylistModal";
 
 export default (props) => {
-    return (
-        <StyledTrack>
-            <div class="track">
-                <span class="track-number">{props.trackNumber}</span>
-                <AddPlaylistModalForm id={props.id} />
-                <span class="track-title">{props.title}</span>
-                <span class="track-artist">{props.artist}</span>
-                <span class="track-length">{props.length}</span>
-            </div>
-        </StyledTrack>)
+  return (
+    <StyledTrack>
+      <div class="track">
+        <span class="track-number">{props.trackNumber}</span>
+        <AddToPlaylistModalForm id={props.id} />
+        <span class="track-title">{props.title}</span>
+        <span class="track-artist">{props.artist}</span>
+        <span class="track-length">{props.length}</span>
+      </div>
+    </StyledTrack>
+  );
 };
 
 const StyledTrack = styled.div`
-	.track {
-        display: flex;
-        justify-content: flex-start;
-        align-items: center;
-        margin: 5px 0;
-        padding: 5px 0;
-        border-bottom: solid 1px rgb(15,30,54, 0.1);
-        height: 40px;
-        width: 100%;
+  .track {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    margin: 5px 0;
+    padding: 5px 0;
+    border-bottom: solid 1px rgb(15, 30, 54, 0.1);
+    height: 40px;
+    width: 100%;
 
-        .track-number {
-            flex: 1;
-            color: #0F1E36;
-            opacity: 50%;
-        }
-
-        .track-add {
-            flex: 1;
-        }
-
-        .track-title {
-            flex: 5;
-            font-size: 0.9rem;
-            color: #0F1E36;
-            opacity: 100%;
-        }
-
-        .track-artist {
-            flex: 5;
-            color: #0F1E36;
-            opacity: 50%;
-        }
-
-        .track-length {
-            flex: 3;
-            color: #0F1E36;
-            opacity: 50%;
-        }
+    .track-number {
+      flex: 1;
+      color: #0f1e36;
+      opacity: 50%;
     }
+
+    .track-add {
+      flex: 1;
+    }
+
+    .track-title {
+      flex: 5;
+      font-size: 0.9rem;
+      color: #0f1e36;
+      opacity: 100%;
+    }
+
+    .track-artist {
+      flex: 5;
+      color: #0f1e36;
+      opacity: 50%;
+    }
+
+    .track-length {
+      flex: 3;
+      color: #0f1e36;
+      opacity: 50%;
+    }
+  }
 `;
