4b525dc8f9238f95ad84fdad69c12e539a7800e7
Matteo Lecuit <matteo@lecuit.fr>
2020-11-20 17:12:25 +0100

    playlists page

--- front/src/pages/Playlists/PlaylistsPage.js
+++ front/src/pages/Playlists/PlaylistsPage.js
@@ -7,57 +7,51 @@ import StyledArtistBanner from "../../components/Commons/ArtistBanner";
 
 class ArtistsPage extends Component {
   state = {
-    user: {
-      name: "John Bovi",
-      img:
-        "https://scontent-cdt1-1.xx.fbcdn.net/v/t1.0-9/50496073_372353200233680_4618796985325977600_n.jpg?_nc_cat=101&ccb=2&_nc_sid=09cbfe&_nc_ohc=_uc12sulTCkAX-_QX8z&_nc_ht=scontent-cdt1-1.xx&oh=9f4ec88556fd1165347daeed0b3099ca&oe=5FD49437",
-      bannerImg:
-        "https://s3-eu-west-1.amazonaws.com/images.linnlive.com/fe50132e61c7590af0f3485de98353dd/66bd9ea3-36f7-4ff6-8ae7-69d50cfca420.jpg",
-      playlists: [
-        {
-          name: "Liked Songs",
-          img:
-            "https://e-cdns-images.dzcdn.net/images/playlist/510b6a39695df5bf96a650394790f2bb/264x264-000000-80-0-0.jpg",
-          url: "/playlists/1",
-        },
-        {
-          name: "Best Of Big Ali",
-          img:
-            "https://upload.wikimedia.org/wikipedia/commons/d/d1/Big_Ali_2011.jpg",
-          url: "/playlists/1",
-        },
-        {
-          name: "Fiesta",
-          img:
-            "https://e-cdns-images.dzcdn.net/images/cover/8f7ff24dbbb77f281bf7f7ae0be72c0c/264x264-000000-80-0-0.jpg",
-          url: "/playlists/1",
-        },
-        {
-          name: "Motivation",
-          img:
-            "https://e-cdns-images.dzcdn.net/images/cover/8f7ff24dbbb77f281bf7f7ae0be72c0c/264x264-000000-80-0-0.jpg",
-          url: "/playlists/1",
-        },
-      ],
-    },
+    playlists: [],
   };
+  getPlaylists() {
+    fetch("http://localhost:8080/utilisateur/playlist/", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `${localStorage.getItem("token")}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((playlists) => {
+        this.setState({ playlists });
+        console.log(this.state.playlists);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  componentDidMount() {
+    this.getPlaylists();
+  }
 
   // List Playlists
-  playlists = this.state.user.playlists.map((item) => (
-    <StyleCard imgUrl={item.img} titre={item.name} link={item.url}></StyleCard>
-  ));
 
   render() {
+    let playlists = [];
+    if (this.state.playlists) {
+      playlists = this.state.playlists.map((item) => {
+        let imageUrl;
+        if (item.titres.length > 0) {
+          imageUrl = item.titres[0].imageUrl;
+        }
+        return (
+          <StyleCard
+            imgUrl={imageUrl}
+            titre={item.nom}
+            link={"/playlists/" + item.id}
+          ></StyleCard>
+        );
+      });
+    }
     return (
       <Container>
         <Row>
           <Col>
-            <StyledArtistBanner
-              bannerImg={this.state.user.bannerImg}
-              img={this.state.user.img}
-              name={this.state.user.name}
-              albumsCount={this.state.user.playlists.length}
-            ></StyledArtistBanner>
             <h2 style={{ margin: "20px 0" }}>Playlists</h2>
             <ul
               style={{
@@ -67,7 +61,7 @@ class ArtistsPage extends Component {
                 flexWrap: "wrap",
               }}
             >
-              {this.playlists}
+              {playlists}
             </ul>
           </Col>
         </Row>
